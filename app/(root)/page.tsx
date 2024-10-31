f2c1635a1cb4ad0932372a073dd27214ef60d38e
rlassissi023 <rlassissi023@student.wethinkcode.co.za>
2024-10-31 19:47:05 +0200

    Created right sideBar and routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,36 @@
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
-    const loggedIn = { firstName: 'Raimi' }
+    const loggedIn = { firstName: 'Raimi', lastName: 'Dikamona', email: 'raimilassissi8@gmail.com' }
 
     return (
         <section className="home">
-      <div className="home-content">
-        <header className="home-header">
-          <HeaderBox 
-            type="greeting"
-            title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
-            subtext="Access and manage your account and transactions efficiently."
-          />
+          <div className="home-content">
+            <header className="home-header">
+              <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account and transactions efficiently."
+              />
 
-          <TotalBalanceBox 
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={12500.35}
+              <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={12500.35}
+              />
+
+              RECENT TRANSACTION
+            </header>
+          </div>
+
+          <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{ currentBalance: 1250.50 }, { currentBalance: 250.50 }]}
           />
-        </header>
-                
-            </div>
         </section>
     )
 }
